35ce278b543d6c347fe1c7c8cc2ec26aaf011062
isolcat <76111612+isolcat@users.noreply.github.com>
2023-09-21 16:32:54 +0800

    Add files via upload

--- 手撕/18_promise.all.js
+++ 手撕/18_promise.all.js
@@ -1,17 +1,37 @@
-Promise.myAll=function(promise){
-    let arr=[]//存储promise的结果
-    count=0//计数器
-    return new Promise((resolve,reject)=>{
-        promise.forEach((item,i)=>{
-            Promise.resolve(item).then(res=>{
-                arr[i]=res
-                count+=1
-                if(count===promise.length){
+Promise.myAll = function (promise) {
+    let arr = []//存储promise的结果
+    count = 0//计数器
+    return new Promise((resolve, reject) => {
+        promise.forEach((item, i) => {
+            Promise.resolve(item).then(res => {
+                arr[i] = res//将解决的结果存储到数组 arr 的对应位置
+                count += 1//计数器+1
+                if (count === promise.length) {//如果所有的Promise都已处理完毕
                     resolve(arr)
                 }
-            }).catch(err=>{
+            }).catch(err => {
                 reject(err)
             })
         })
     })
 }
+
+// test
+let p1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve(1)
+    }, 1000)
+})
+let p2 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve(2)
+    }, 2000)
+})
+let p3 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve(3)
+    }, 3000)
+})
+Promise.myAll([p3, p1, p2]).then(res => {
+    console.log(res) // [3, 1, 2]
+})
