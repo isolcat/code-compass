a73b07f81fb27665093dee6b74b40d67193f16c7
isolcat <76111612+isolcat@users.noreply.github.com>
2023-10-29 21:32:54 +0800

    Update 22_浅拷贝.js

--- 手撕/22_浅拷贝.js
+++ 手撕/22_浅拷贝.js
@@ -1,32 +1,10 @@
-function shallowCopy(object) {
-    // 只拷贝对象
-    if (!object || typeof object !== "object") return;
+var obj = { a: {a: "hello", b: 21} };
+var initalObj = Object.assign({}, obj);
 
-    // 根据 object 的类型判断是新建一个数组还是对象
-    let newObject = Array.isArray(object) ? [] : {};
+initalObj.a.a = "changed";
+console.log(obj.a.a); // "changed"
 
-    // 遍历 object，并且判断是 object 的属性才拷贝
-    for (let key in object) {
-        newObject[key] = object[key];
-    }
-
-    return newObject;
-}
-
-const originalObj = {
-    a: 1,
-    b: {
-        c: 2,
-        d: [3, 4],
-    },
-};
-
-const clonedObj = shallowCopy(originalObj);
-
-clonedObj.b.c = 5;
-clonedObj.a= 5;
-clonedObj.b.d[0] = 6;
-
-//结果会互相干扰
-console.log(originalObj); // { a: 1, b: { c: 5, d: [ 6, 4 ] } }
-console.log(clonedObj); // {a: 1, b: {c: 5, d: [6, 4]}}
+let obj1 = {a: 1}
+let obj2 = {...obj1}
+obj1.a = 2
+console.log(obj2.a)   // 1
