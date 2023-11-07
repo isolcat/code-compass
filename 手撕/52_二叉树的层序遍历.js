28d8f78660c38e76f1e1878c5d3631a1d66247be
isolcat <76111612+isolcat@users.noreply.github.com>
2023-11-07 16:40:50 +0800

    Add files via upload

--- 手撕/52_二叉树的层序遍历.js
+++ 手撕/52_二叉树的层序遍历.js
@@ -1,40 +1,41 @@
-// 定义二叉树节点
-function TreeNode(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
 }
 
 // 层序遍历函数
-function levelOrderTraversal(root) {
+function levelNode(root) {
     if (!root) {
         return [];
     }
 
-    const result = [];
+    const res = []; // 存储层序遍历的结果
     const queue = [root]; // 使用队列来存储待访问的节点
 
     while (queue.length > 0) {
-        const currentLevelValues = [];
-        const levelSize = queue.length;
+        const level = []; // 存储当前层级的节点值
+        const size = queue.length; // 当前层级的节点数量
 
-        for (let i = 0; i < levelSize; i++) {
-            const node = queue.shift(); // 出队列
-            currentLevelValues.push(node.val);
+        for (let i = 0; i < size; i++) {
+            const node = queue.shift(); // 出队列，取出当前层级的节点
+            level.push(node.val); // 将节点的值加入当前层级的数组
 
             if (node.left) {
-                queue.push(node.left); // 左子节点入队列
+                queue.push(node.left); // 左子节点入队列，以便后续遍历
             }
 
             if (node.right) {
-                queue.push(node.right); // 右子节点入队列
+                queue.push(node.right); // 右子节点入队列，以便后续遍历
             }
         }
 
-        result.push(currentLevelValues); // 当前层级的数值
+        res.push(level); // 将当前层级的节点值数组加入结果数组
     }
 
-    return result;
+    return res; // 返回层序遍历的结果，每个子数组表示一个层级的节点值
 }
 
 // 创建一个示例二叉树
@@ -46,5 +47,5 @@ root.left.right = new TreeNode(5);
 root.right.right = new TreeNode(6);
 
 // 执行层序遍历
-const result = levelOrderTraversal(root);
-console.log(result); // 输出每层的节点值
+const result = levelNode(root);
+console.log(result);
